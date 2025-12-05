7ac5a5c70d558bfc8421e943fbaa7efb7ba9a9a3
本川　晴彦 <harx6009@icloud.com>
2025-12-06 01:04:58 +0900

    feat: デバイスセキュリティ状態のProviderを追加

---
+++ lib/data/repositories/device_security/providers/device_security_status_provider.dart
@@ -0,0 +1,12 @@
+import 'package:freerasp_sample/data/repositories/device_security/providers/device_security_repository_provider.dart';
+import 'package:freerasp_sample/domains/value_object/device_security_status.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'device_security_status_provider.g.dart';
+
+/// デバイスセキュリティ状態のProvider
+@Riverpod(keepAlive: true)
+Stream<DeviceSecurityStatus> deviceSecurityStatus(Ref ref) {
+  final repository = ref.read(deviceSecurityRepositoryProvider);
+  return repository.statusStream;
+}
