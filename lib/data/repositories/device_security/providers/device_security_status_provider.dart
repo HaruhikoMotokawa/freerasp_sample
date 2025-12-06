463e5041c51141d5b84990236c73a85a7c242cd9
本川　晴彦 <harx6009@icloud.com>
2025-12-06 09:11:01 +0900

    feat: デバイスセキュリティ状態のNotifierクラスを追加し、通知ロジックを実装

--- lib/data/repositories/device_security/providers/device_security_status_provider.dart
+++ lib/data/repositories/device_security/providers/device_security_status_provider.dart
@@ -6,7 +6,26 @@ part 'device_security_status_provider.g.dart';
 
 /// デバイスセキュリティ状態のProvider
 @Riverpod(keepAlive: true)
-Stream<DeviceSecurityStatus> deviceSecurityStatus(Ref ref) {
-  final repository = ref.read(deviceSecurityRepositoryProvider);
-  return repository.statusStream;
+class DeviceSecurityStatusNotifier extends _$DeviceSecurityStatusNotifier {
+  @override
+  Stream<DeviceSecurityStatus> build() {
+    final repository = ref.read(deviceSecurityRepositoryProvider);
+    return repository.statusStream;
+  }
+
+  @override
+  bool updateShouldNotify(
+    AsyncValue<DeviceSecurityStatus> previous,
+    AsyncValue<DeviceSecurityStatus> next,
+  ) {
+    if (next.hasError || next.isLoading) return false;
+
+    // もし一度でも不正が流れてきてきた場合、次にどんな値が流れてきても通知しない
+    if (previous.value case final DeviceSecurityStatusThreat _) {
+      return false;
+    }
+
+    // 基本は常に通知する
+    return true;
+  }
 }
